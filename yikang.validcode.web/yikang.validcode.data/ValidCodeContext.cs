0d175cda687ef72cbea50cc9c76a3e4826f679ea
zuoqi <491127203@qq.com>
2018-05-05 16:10:24 +0800

    x

---
+++ yikang.validcode.web/yikang.validcode.data/ValidCodeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yikang.validcode.data.co;
+
+namespace yikang.validcode.data
+{
+    public class ValidCodeContext : DbContext
+    {
+        public ValidCodeContext():base("validcode")
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
