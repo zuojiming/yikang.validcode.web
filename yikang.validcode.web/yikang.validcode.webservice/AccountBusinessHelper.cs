9d4f7f6420e6b6ed1540c09e5da31016dcfa69c2
zuoqi <491127203@qq.com>
2018-05-05 21:29:29 +0800

    xx

---
+++ yikang.validcode.web/yikang.validcode.webservice/AccountBusinessHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yikang.validcode.webservice
+{
+    public class AccountBusinessHelper
+    {
+        private static AccountBusinessHelper sine_ { get; set; }
+        public static AccountBusinessHelper Initialize()
+        {
+            if (sine_ == null) {
+                sine_ = new AccountBusinessHelper();
+            }
+            return sine_;
+        }
+        private AccountBusinessHelper()
+        {
+
+        }
+    }
+}
