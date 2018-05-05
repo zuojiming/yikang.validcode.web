70ea86e659ce8a0e01462fd34872bd7d72fa1200
zuoqi <491127203@qq.com>
2018-05-05 21:21:15 +0800

    x

---
+++ yikang.validcode.web/yikang.validcode.webservice/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yikang.validcode.webservice
+{
+    public class Class1
+    {
+    }
+}
